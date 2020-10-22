8e9c9d6a21229bd9dc42b709c1c2a31be5287a18
Gurcan Hamali <gurcanhamali@gmail.com>
2020-10-22 13:33:25 +1100

    Added @input and @output

--- src/app/greeter.component.ts
+++ src/app/greeter.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  // selector: 'do-greet',
+  // selector: "do-greet",
   template: `
-    <div>
-      Hi there!
-    </div>
+    <div>Hi {{ name }}!</div>
+    <button (click)="doGreet()">Greet!</button>
   `,
-  styles: []
+  styles: [],
 })
 export class GreeterComponent implements OnInit {
+  @Input() name: string;
+  @Output() greet = new EventEmitter();
+
   constructor() {}
 
   ngOnInit() {}
+  doGreet() {
+    this.greet.emit(`Hi, ${this.name}`);
+  }
 }
